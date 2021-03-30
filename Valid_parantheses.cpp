2243a98427f4f4da9fdc87015880912216ee86fe
Om Khare <omkhare02@gmail.com>
2021-03-30 11:05:06 +0530

    Valid Parantheses using stack

---
+++ Valid_parantheses.cpp
@@ -0,0 +1,19 @@
+#include <stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char& c: s){
+            switch (c){
+                case '(': st.push(c); break;
+                case '[': st.push(c); break;
+                case '{': st.push(c); break;
+                case ')': if(st.empty() || st.top()!='(') return false; else st.pop(); break;
+                case ']': if(st.empty() || st.top()!='[') return false; else st.pop(); break;
+                case '}': if(st.empty() || st.top()!='{') return false; else st.pop(); break;
+                default: ;
+            }
+        }
+        return st.empty();
+    }
+};
